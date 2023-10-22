eddb4f0a639c3877692b32783da3e76a08e4514c
KyllianBeguin <KyllianBeguin@vivaldi.net>
2023-10-22 17:35:40 +0000

    [kb] 📄 Add comments

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	// read_dir handle the files renaming
 	"change_files_name/pkg/read_dir"
 )
 
 func main() {
+	// Ask for the prefix
 	my_prefix := read_dir.AskFilePrefix()
+
+	// Rename every files in the current folder
 	read_dir.RenameFiles(my_prefix)
 }
